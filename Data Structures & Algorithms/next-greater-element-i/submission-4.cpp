b405913df9a67c8a7bb27570c152c152e47be40a
Tanvi <159919214+TanviKrishnan2005@users.noreply.github.com>
2026-08-08 20:02:11 +0530

    Add: next-greater-element-i - submission-4

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        vector<int>res(n,-1);
+        stack<int>st;
+        unordered_map<int,int>mp;
+        for(int i =0;i<n;i++){
+            mp[nums1[i]]=i;
+        }
+        for(int num :nums2){
+            while(!st.empty()&& num>st.top()){
+                int val = st.top();
+                st.pop();
+                int idx = mp[val];
+                res[idx]=num;
+            }
+            if(mp.find(num)!=mp.end()){
+                st.push(num);
+            }
+        }
+        return res;
+    }
+};
